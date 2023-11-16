d686b27454433ee536fa9f6f12b53b1fa1dec060
dhanush-bhargav <dhanushsbhargav@gmail.com>
2023-11-15 22:28:21 -0500

    clean and split data

--- code/get_data.py
+++ code/get_data.py
@@ -1,3 +1,8 @@
 import pandas as pd
 
 raw_data = pd.read_csv("data/card_transdata.csv")
+
+if raw_data.isna().sum().sum() == 0:
+    print("No missing data in the dataset")
+
+print(raw_data.median())
---
+++ code/split_data.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def get_split_normalized_data(dataset):
+    features = dataset.drop(columns=["fraud"])
+    labels = dataset["fraud"]
+    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.8, random_state=0)
+
+    mean_dist_from_home, sd_dist_from_home = X_train["distance_from_home"].mean(), X_train["distance_from_home"].sd()
+    mean_dist_from_last, sd_dist_from_last = X_train["distance_from_last_transaction"].mean(), X_train["distance_from_last_transaction"].sd()
+    mean_ratio_to_median, sd_ratio_to_median = X_train["ratio_to_median_purchase_price"].mean(), X_train["ratio_to_median_purchase_price"].sd() 
+
+    X_train["distance_from_home"] = (X_train["distance_from_home"] - mean_dist_from_home) / sd_dist_from_home
+    X_train["distance_from_last_transaction"] = ( X_train["distance_from_last_transaction"] - mean_dist_from_last) / sd_dist_from_last
+    X_train["ratio_to_median_purchase_price"] = (X_train["ratio_to_median_purchase_price"] - mean_ratio_to_median) / sd_ratio_to_median
+
+    
