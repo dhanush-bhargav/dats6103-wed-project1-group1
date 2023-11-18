42318dcf7c7725075c88968afd7e8d498f423e20
dineshanandt <143751926+dineshanandt@users.noreply.github.com>
2023-11-18 18:36:21 -0500

    Initial

--- code/get_data.py
+++ code/get_data.py
@@ -1,3 +1,21 @@
+#%%
 import pandas as pd
 
-raw_data = pd.read_csv("data/card_transdata.csv")
+df = raw_data = pd.read_csv("/Users/dineshanandthulasiraman/Documents/GitHub/dats6103-wed-project1-group1/data/card_transdata.csv")
+
+#%%
+#Data Inspection
+print(df.head())
+
+#checking information of data
+df.info()
+
+#checking null values
+df.isnull().sum()
+
+#describing data
+df.describe().transpose()
+
+#checking 1st Row
+df.iloc[0,:]
+#%%
