8731814db7796d5e0fdd0369c41516bf88de6ae8
dineshanandt <143751926+dineshanandt@users.noreply.github.com>
2023-11-22 21:13:34 -0500

    EDA

--- code/get_data.py
+++ code/get_data.py
@@ -56,6 +56,48 @@ for feature in num_features:
     plt.title(f'Boxplot for {feature}')
     plt.show()
 # %%
+# Correlation heatmap
+correlation_matrix = df.corr()
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
+plt.title('Correlation Heatmap')
+plt.show()
 
+#%%
 
+# Relationship between 'distance_from_home' and 'distance_from_last_transaction'
+plt.figure(figsize=(8, 6))
+sns.scatterplot(x='distance_from_home', y='distance_from_last_transaction', hue='fraud', data=df)
+plt.title('Relationship between Distance from Home and Last Transaction')
+plt.xlabel('Distance from Home')
+plt.ylabel('Distance from Last Transaction')
+plt.show()
 
+#%%
+# Analysis of 'ratio_to_median_purchase_price'
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+sns.boxplot(x='fraud', y='ratio_to_median_purchase_price', data=df)
+plt.title('Boxplot of Ratio to Median Purchase Price')
+
+plt.subplot(1, 2, 2)
+sns.histplot(df['ratio_to_median_purchase_price'], bins=30, kde=True)
+plt.title('Distribution of Ratio to Median Purchase Price')
+plt.xlabel('Ratio to Median Purchase Price')
+
+plt.tight_layout(rect=[0, 0, 1, 0.96])
+plt.show()
+
+#%%
+# Analysis of 'repeat_retailer' and 'fraud'
+sns.countplot(x='repeat_retailer', hue='fraud', data=df)
+plt.title('Count of Fraudulent Transactions based on Repeated Retailer')
+plt.xlabel('Repeat Retailer')
+plt.show()
+
+#%%
+# Pairplot with a focus on 'fraud'
+sns.pairplot(df, hue='fraud', vars=['distance_from_home', 'distance_from_last_transaction', 'ratio_to_median_purchase_price'])
+plt.suptitle('Pairplot with a Focus on Fraud')
+plt.show()
+
+# %%
