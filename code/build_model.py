57ee0794ee645c790eb7b0fa39ae44290d2d126f
dhanush-bhargav <dhanushsbhargav@gmail.com>
2023-12-01 13:30:57 -0500

    code for modelling

---
+++ code/build_model.py
@@ -0,0 +1,41 @@
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+import split_data
+import pandas as pd
+
+
+def build_LR_model(X_train, y_train):
+    lr_model = LogisticRegressionCV(cv=10, scoring="recall").fit(X_train, y_train)
+    return lr_model
+
+def build_KNN_model(X_train, y_train, k):
+    knn_model = KNeighborsClassifier(n_neighbors=k).fit(X_train, y_train)
+    return knn_model
+
+def build_forest_model(X_train, y_train, estimators, depth):
+    RF_model = RandomForestClassifier(n_estimators=estimators, max_depth=depth).fit(X_train, y_train)
+    return RF_model
+
+def build_svc_model(X_train, y_train):
+    svc_model = SVC(kernel='linear').fit(X_train, y_train)
+    return svc_model
+
+
+if __name__=="__main__":
+    dataset = pd.read_csv("data/card_transdata.csv")
+    X_train, X_test, y_train, y_test = split_data.get_split_normalized_data(dataset)
+    lrcv_model = build_LR_model(X_train, y_train)
+    
+    knn_model1 = build_KNN_model(X_train, y_train, 10)
+    knn_model2 = build_KNN_model(X_train, y_train, 100)
+    knn_model3 = build_KNN_model(X_train, y_train, 250)
+
+    rf_model1 = build_forest_model(X_train, y_train, 100, 20)
+    rf_model2 = build_forest_model(X_train, y_train, 100, 10)
+
+    rf_model3 = build_forest_model(X_train, y_train, 50, 20)
+    rf_model4 = build_forest_model(X_train, y_train, 50, 10)
+
+    svc_model = build_svc_model(X_train, y_train)
--- code/split_data.py
+++ code/split_data.py
@@ -6,9 +6,9 @@ def get_split_normalized_data(dataset, ratio=0.8):
     labels = dataset["fraud"]
     X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=ratio, random_state=0)
 
-    mean_dist_from_home, sd_dist_from_home = X_train["distance_from_home"].mean(), X_train["distance_from_home"].sd()
-    mean_dist_from_last, sd_dist_from_last = X_train["distance_from_last_transaction"].mean(), X_train["distance_from_last_transaction"].sd()
-    mean_ratio_to_median, sd_ratio_to_median = X_train["ratio_to_median_purchase_price"].mean(), X_train["ratio_to_median_purchase_price"].sd() 
+    mean_dist_from_home, sd_dist_from_home = X_train["distance_from_home"].mean(), X_train["distance_from_home"].std()
+    mean_dist_from_last, sd_dist_from_last = X_train["distance_from_last_transaction"].mean(), X_train["distance_from_last_transaction"].std()
+    mean_ratio_to_median, sd_ratio_to_median = X_train["ratio_to_median_purchase_price"].mean(), X_train["ratio_to_median_purchase_price"].std() 
 
     X_train["distance_from_home"] = (X_train["distance_from_home"] - mean_dist_from_home) / sd_dist_from_home
     X_train["distance_from_last_transaction"] = ( X_train["distance_from_last_transaction"] - mean_dist_from_last) / sd_dist_from_last
