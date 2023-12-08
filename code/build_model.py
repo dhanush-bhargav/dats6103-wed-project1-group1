dedde920c7a355770c2a6f6a16d72923a74c0d23
dhanush-bhargav <dhanushsbhargav@gmail.com>
2023-12-07 19:14:25 -0500

    add LR from statsmodels

--- code/build_model.py
+++ code/build_model.py
@@ -2,6 +2,7 @@ from sklearn.linear_model import LogisticRegressionCV
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
+from statsmodels.formula.api import logit
 import split_data
 import pandas as pd
 
@@ -26,16 +27,19 @@ def build_svc_model(X_train, y_train):
 if __name__=="__main__":
     dataset = pd.read_csv("data/card_transdata.csv")
     X_train, X_test, y_train, y_test = split_data.get_split_normalized_data(dataset)
-    lrcv_model = build_LR_model(X_train, y_train)
+    # lrcv_model = build_LR_model(X_train, y_train)
     
-    knn_model1 = build_KNN_model(X_train, y_train, 10)
-    knn_model2 = build_KNN_model(X_train, y_train, 100)
-    knn_model3 = build_KNN_model(X_train, y_train, 250)
+    # knn_model1 = build_KNN_model(X_train, y_train, 10)
+    # knn_model2 = build_KNN_model(X_train, y_train, 100)
+    # knn_model3 = build_KNN_model(X_train, y_train, 250)
 
-    rf_model1 = build_forest_model(X_train, y_train, 100, 20)
-    rf_model2 = build_forest_model(X_train, y_train, 100, 10)
+    # rf_model1 = build_forest_model(X_train, y_train, 100, 20)
+    # rf_model2 = build_forest_model(X_train, y_train, 100, 10)
 
-    rf_model3 = build_forest_model(X_train, y_train, 50, 20)
-    rf_model4 = build_forest_model(X_train, y_train, 50, 10)
+    # rf_model3 = build_forest_model(X_train, y_train, 50, 20)
+    # rf_model4 = build_forest_model(X_train, y_train, 50, 10)
 
-    svc_model = build_svc_model(X_train, y_train)
+    # svc_model = build_svc_model(X_train, y_train)
+
+    logit_model = logit("fraud ~ distance_from_home + distance_from_last_transaction + ratio_to_median_purchase_price + repeat_retailer + used_chip + used_pin_number + online_order", dataset).fit()
+    print(logit_model.summary())
