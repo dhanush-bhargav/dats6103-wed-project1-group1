86e50ebbf44b130ca6b022469e9cad4b8167cee3
Manoj Padala <manojpadala1634@gmail.com>
2023-12-11 02:17:56 -0500

    Update split_data.py

--- code/split_data.py
+++ code/split_data.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+
 def get_split_normalized_data(dataset, ratio=0.8):
     features = dataset.drop(columns=["fraud"])
     labels = dataset["fraud"]
-    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=ratio, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(features, labels, train_size=ratio, random_state=0)
 
     mean_dist_from_home, sd_dist_from_home = X_train["distance_from_home"].mean(), X_train["distance_from_home"].std()
     mean_dist_from_last, sd_dist_from_last = X_train["distance_from_last_transaction"].mean(), X_train["distance_from_last_transaction"].std()
